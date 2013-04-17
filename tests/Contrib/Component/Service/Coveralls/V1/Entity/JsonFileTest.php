94617821ae6db35b478c789482eb9028db952fcf
Kitamura Satoshi <with.no.parachute@gmail.com>
2013-04-17 21:05:09 +0900

    refactor test

--- tests/Contrib/Component/Service/Coveralls/V1/Entity/JsonFileTest.php
+++ tests/Contrib/Component/Service/Coveralls/V1/Entity/JsonFileTest.php
@@ -1,20 +1,115 @@
 <?php
 namespace Contrib\Component\Service\Coveralls\V1\Entity;
 
+use Contrib\Component\Service\Coveralls\V1\Collector\CloverXmlCoverageCollector;
+
+/**
+ * @covers Contrib\Component\Service\Coveralls\V1\Entity\JsonFile
+ *
+ * @author Kitamura Satoshi <with.no.parachute@gmail.com>
+ */
 class JsonFileTest extends \PHPUnit_Framework_TestCase
 {
     protected function setUp()
     {
-        $this->path = __DIR__ . '/files/test.php';
+        $this->dir      = realpath(__DIR__ . '/../../');
+        $this->rootDir  = $this->dir . '/V1/Entity/files/';
+        $this->path     = __DIR__ . '/files/test.php';
         $this->filename = 'test.php';
 
         $this->object = new JsonFile();
     }
 
+
+
     protected function createSourceFile()
     {
         return new SourceFile($this->path, $this->filename);
     }
+
+    protected function getCloverXml()
+    {
+        $xml = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<coverage generated="1365848893">
+  <project timestamp="1365848893">
+    <file name="%s/V1/Entity/files/test.php">
+      <class name="TestFile" namespace="global">
+        <metrics methods="1" coveredmethods="0" conditionals="0" coveredconditionals="0" statements="1" coveredstatements="0" elements="2" coveredelements="0"/>
+      </class>
+      <line num="5" type="method" name="__construct" crap="1" count="0"/>
+      <line num="7" type="stmt" count="0"/>
+    </file>
+    <file name="dummy.php">
+      <class name="TestFile" namespace="global">
+        <metrics methods="1" coveredmethods="0" conditionals="0" coveredconditionals="0" statements="1" coveredstatements="0" elements="2" coveredelements="0"/>
+      </class>
+      <line num="5" type="method" name="__construct" crap="1" count="0"/>
+      <line num="7" type="stmt" count="0"/>
+    </file>
+    <package name="Hoge">
+      <file name="%s/V1/Entity/files/test2.php">
+        <class name="TestFile" namespace="Hoge">
+          <metrics methods="1" coveredmethods="0" conditionals="0" coveredconditionals="0" statements="1" coveredstatements="0" elements="2" coveredelements="0"/>
+        </class>
+        <line num="6" type="method" name="__construct" crap="1" count="0"/>
+        <line num="8" type="stmt" count="0"/>
+      </file>
+    </package>
+  </project>
+</coverage>
+XML;
+        return sprintf($xml, $this->dir, $this->dir);
+    }
+
+    protected function createCloverXml()
+    {
+        $xml = $this->getCloverXml();
+
+        return simplexml_load_string($xml);
+    }
+
+    protected function collectJsonFile()
+    {
+        $xml       = $this->createCloverXml();
+        $collector = new CloverXmlCoverageCollector();
+
+        return $collector->collect($xml, $this->rootDir);
+    }
+
+    protected function getNoSourceCloverXml()
+    {
+        return <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<coverage generated="1365848893">
+  <project timestamp="1365848893">
+    <file name="dummy.php">
+      <class name="TestFile" namespace="global">
+        <metrics methods="1" coveredmethods="0" conditionals="0" coveredconditionals="0" statements="1" coveredstatements="0" elements="2" coveredelements="0"/>
+      </class>
+      <line num="5" type="method" name="__construct" crap="1" count="0"/>
+      <line num="7" type="stmt" count="0"/>
+    </file>
+  </project>
+</coverage>
+XML;
+    }
+
+    protected function createNoSourceCloverXml()
+    {
+        $xml = $this->getNoSourceCloverXml();
+
+        return simplexml_load_string($xml);
+    }
+
+    protected function collectJsonFileWithoutSourceFiles()
+    {
+        $xml       = $this->createNoSourceCloverXml();
+        $collector = new CloverXmlCoverageCollector();
+
+        return $collector->collect($xml, $this->rootDir);
+    }
+
 
     // getServiceName()
 
@@ -54,6 +149,26 @@ class JsonFileTest extends \PHPUnit_Framework_TestCase
     public function shouldNotHaveServiceJobIdOnConstruction()
     {
         $this->assertNull($this->object->getServiceJobId());
+    }
+
+    // getServiceNumber()
+
+    /**
+     * @test
+     */
+    public function shouldNotHaveServiceNumberOnConstruction()
+    {
+        $this->assertNull($this->object->getServiceNumber());
+    }
+
+    // getServiceEventType()
+
+    /**
+     * @test
+     */
+    public function shouldNotHaveServiceEventTypeOnConstruction()
+    {
+        $this->assertNull($this->object->getServiceEventType());
     }
 
     // getGit()
@@ -316,5 +431,162 @@ class JsonFileTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected, $object->toArray());
         $this->assertEquals(json_encode($expected), (string)$object);
+    }
+
+
+    // fillJobs()
+
+    /**
+     * @test
+     */
+    public function sendTravisCiJob()
+    {
+        $serviceName  = 'travis-ci';
+        $serviceJobId = '1.1';
+
+        $env = array();
+        $env['TRAVIS']        = true;
+        $env['TRAVIS_JOB_ID'] = $serviceJobId;
+
+        $object = $this->collectJsonFile();
+
+        $same = $object->fillJobs($env);
+
+        $this->assertSame($same, $object);
+        $this->assertEquals($serviceName, $object->getServiceName());
+        $this->assertEquals($serviceJobId, $object->getServiceJobId());
+    }
+
+    /**
+     * @test
+     */
+    public function sendTravisProJob()
+    {
+        $serviceName  = 'travis-pro';
+        $serviceJobId = '1.2';
+
+        $env = array();
+        $env['TRAVIS']        = true;
+        $env['TRAVIS_JOB_ID'] = $serviceJobId;
+
+        $object = $this
+        ->collectJsonFile()
+        ->setServiceName($serviceName);
+
+        $same = $object->fillJobs($env);
+
+        $this->assertSame($same, $object);
+        $this->assertEquals($serviceName, $object->getServiceName());
+        $this->assertEquals($serviceJobId, $object->getServiceJobId());
+    }
+
+    /**
+     * @test
+     */
+    public function sendCircleCiJob()
+    {
+        $serviceName   = 'circleci';
+        $serviceNumber = '123';
+
+        $env = array();
+        $env['CIRCLECI']         = true;
+        $env['CIRCLE_BUILD_NUM'] = $serviceNumber;
+
+        $object = $this->collectJsonFile();
+
+        $same = $object->fillJobs($env);
+
+        $this->assertSame($same, $object);
+        $this->assertEquals($serviceName, $object->getServiceName());
+        $this->assertEquals($serviceNumber, $object->getServiceNumber());
+    }
+
+    /**
+     * @test
+     */
+    public function sendJenkinsJob()
+    {
+        $serviceName   = 'jenkins';
+        $serviceNumber = '123';
+
+        $env = array();
+        $env['JENKINS_URL']  = 'http://localhost:8080';
+        $env['BUILD_NUMBER'] = $serviceNumber;
+
+        $object = $this->collectJsonFile();
+
+        $same = $object->fillJobs($env);
+
+        $this->assertSame($same, $object);
+        $this->assertEquals($serviceName, $object->getServiceName());
+        $this->assertEquals($serviceNumber, $object->getServiceNumber());
+    }
+
+    /**
+     * @test
+     */
+    public function sendLocalJob()
+    {
+        $serviceName      = 'php-coveralls';
+        $serviceEventType = 'manual';
+
+        $env = array();
+        $env['COVERALLS_RUN_LOCALLY'] = true;
+
+        $object = $this->collectJsonFile();
+
+        $same = $object->fillJobs($env);
+
+        $this->assertSame($same, $object);
+        $this->assertEquals($serviceName, $object->getServiceName());
+        $this->assertNull($object->getServiceJobId());
+        $this->assertEquals($serviceEventType, $object->getServiceEventType());
+    }
+
+    /**
+     * @test
+     */
+    public function sendUnsupportedJob()
+    {
+        $repoToken = 'token';
+
+        $env = array();
+        $env['COVERALLS_REPO_TOKEN'] = $repoToken;
+
+        $object = $this->collectJsonFile();
+
+        $same = $object->fillJobs($env);
+
+        $this->assertSame($same, $object);
+        $this->assertEquals($repoToken, $object->getRepoToken());
+    }
+
+
+    /**
+     * @test
+     * @expectedException RuntimeException
+     */
+    public function throwRuntimeExceptionIfInvalidEnv()
+    {
+        $env = array();
+
+        $object = $this->collectJsonFile();
+
+        $object->fillJobs($env);
+    }
+
+    /**
+     * @test
+     * @expectedException RuntimeException
+     */
+    public function throwRuntimeExceptionIfNoSourceFiles()
+    {
+        $env = array();
+        $env['TRAVIS']        = true;
+        $env['TRAVIS_JOB_ID'] = '1.1';
+
+        $object = $this->collectJsonFileWithoutSourceFiles();
+
+        $object->fillJobs($env);
     }
 }
