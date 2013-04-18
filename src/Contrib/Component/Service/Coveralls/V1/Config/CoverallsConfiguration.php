75170a145e47e3d0b87d907cae564d33f1f0b9e8
Kitamura Satoshi <with.no.parachute@gmail.com>
2013-04-18 20:09:14 +0900

    rm commented lines

--- src/Contrib/Component/Service/Coveralls/V1/Config/CoverallsConfiguration.php
+++ src/Contrib/Component/Service/Coveralls/V1/Config/CoverallsConfiguration.php
@@ -31,47 +31,6 @@ class CoverallsConfiguration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        /*
-        $file    = new Path();
-        $rootDir = $this->rootDir; // for PHP 5.3 compatibility
-
-        // closures for the configuration value converter
-
-        $realpath = function ($path) use ($file, $rootDir) {
-            return $file->getRealPath($path, $rootDir);
-        };
-
-        $realWritingPath = function ($path) use ($file, $rootDir) {
-            return $file->getRealWritingFilePath($path, $rootDir);
-        };
-
-        // closures for validation
-
-        $fileNotReadable = function ($path) use ($file, $rootDir) {
-            $realpath = $file->getRealPath($path, $rootDir);
-
-            return !$file->isRealFileReadable($realpath);
-        };
-
-        $dirNotFound = function ($path) use ($file, $rootDir) {
-            $realpath = $file->getRealPath($path, $rootDir);
-
-            return !$file->isRealDirExist($realpath);
-        };
-
-        $fileNotWritable = function ($path) use ($file, $rootDir) {
-            $realpath = $file->getRealPath($path, $rootDir);
-
-            if ($realpath !== false) {
-                return !$file->isRealFileWritable($realpath);
-            }
-
-            $realDir = $file->getRealDir($path, $rootDir);
-
-            return !$file->isRealDirWritable($realDir);
-        };
-        */
-
         // define configuration
 
         $treeBuilder = new TreeBuilder();
@@ -93,42 +52,12 @@ class CoverallsConfiguration implements ConfigurationInterface
                 // for php lib
                 ->scalarNode('src_dir')
                     ->defaultValue('src')
-                    /*
-                    ->defaultValue($file->getRealPath('src', $rootDir))
-                    ->validate()
-                        ->always($realpath)
-                    ->end()
-                    ->validate()
-                        ->ifTrue($dirNotFound)
-                            ->thenInvalid('src directory is not found')
-                    ->end()
-                    */
                 ->end()
                 ->scalarNode('coverage_clover')
                     ->defaultValue('build/logs/clover.xml')
-                    /*
-                    ->defaultValue($file->getRealPath('build/logs/clover.xml', $rootDir))
-                    ->validate()
-                        ->always($realpath)
-                    ->end()
-                    ->validate()
-                        ->ifTrue($fileNotReadable)
-                            ->thenInvalid('coverage_clover XML file is not readable')
-                    ->end()
-                    */
                 ->end()
                 ->scalarNode('json_path')
                     ->defaultValue('build/logs/coveralls-upload.json')
-                    /*
-                    ->defaultValue($file->getRealWritingFilePath('build/logs/coveralls-upload.json', $rootDir))
-                    ->validate()
-                        ->always($realWritingPath)
-                    ->end()
-                    ->validate()
-                        ->ifTrue($fileNotWritable)
-                            ->thenInvalid('json_path is not writable')
-                    ->end()
-                    */
                 ->end()
             ->end();
 
