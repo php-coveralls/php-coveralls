9f27e5e0b9eda0bcf0be8205f0e4dd82271e9791
Felipe Martins <me@fefas.net>
2016-04-15 15:07:56 -0300

    add test to the new command line option --json_path

--- tests/Satooshi/Bundle/CoverallsV1Bundle/Config/ConfiguratorTest.php
+++ tests/Satooshi/Bundle/CoverallsV1Bundle/Config/ConfiguratorTest.php
@@ -239,6 +239,38 @@ class ConfiguratorTest extends ProjectTestCase
     /**
      * @test
      */
+    public function shouldLoadJsonPathOverriddenByInput()
+    {
+        $this->makeProjectDir($this->srcDir, $this->logsDir, $this->cloverXmlPath);
+
+        $path = realpath(__DIR__ . '/yaml/json_path.yml');
+
+        // Mocking command line options.
+        $defs = new InputDefinition(
+            array(
+                new InputOption(
+                    'json_path',
+                    'j',
+                    InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY
+                ),
+            )
+        );
+
+        $inputArray = array(
+            '--json_path' => array(
+                'build/logs/coveralls-upload-custom.json',
+            ),
+        );
+        $expectedJsonPath = substr($this->jsonPath, 0, strlen($this->jsonPath) - 5) . '-custom.json';
+
+        $input = new ArrayInput($inputArray, $defs);
+        $config = $this->object->load($path, $this->rootDir, $input);
+        $this->assertConfiguration($config, array($this->cloverXmlPath), $expectedJsonPath);
+    }
+
+    /**
+     * @test
+     */
     public function shouldLoadExcludeNoStmtYmlContainingTrue()
     {
         $this->makeProjectDir($this->srcDir, $this->logsDir, $this->cloverXmlPath);
