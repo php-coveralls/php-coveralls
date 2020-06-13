743818d5353a9318a5e68f31686fd4d9b83a6f78
twinh <twinhuang@qq.com>
2020-06-13 18:22:09 +0800

    refactor: use GITHUB_RUN_ID as CI_JOB_ID, same as coverallsapp/github-action
    (cherry picked from commit 1d3090177f57799ded3897b6f52c2c8be0340594)

--- src/Bundle/CoverallsBundle/Collector/CiEnvVarsCollector.php
+++ src/Bundle/CoverallsBundle/Collector/CiEnvVarsCollector.php
@@ -127,24 +127,20 @@ class CiEnvVarsCollector
         $this->env['CI_NAME'] = 'github';
 
         $githubEventName = $this->env['GITHUB_EVENT_NAME'];
-        $githubSha = $this->env['GITHUB_SHA'];
         $githubRef = $this->env['GITHUB_REF'];
 
         if (strpos($githubRef, 'refs/heads/') !== false) {
             $githubRef = str_replace('refs/heads/', '', $githubRef);
-            $jobId = $githubSha;
         } elseif ($githubEventName === 'pull_request') {
             $refParts = explode('/', $githubRef);
             $prNumber = $refParts[2];
             $this->env['CI_PULL_REQUEST'] = $prNumber;
             $this->readEnv['CI_PULL_REQUEST'] = $this->env['CI_PULL_REQUEST'];
-            $jobId = sprintf('%s-PR-%s', $githubSha, $prNumber);
         } elseif (strpos($githubRef, 'refs/tags/') !== false) {
             $githubRef = str_replace('refs/tags/', '', $githubRef);
-            $jobId = $githubRef;
         }
 
-        $this->env['CI_JOB_ID'] = $jobId;
+        $this->env['CI_JOB_ID'] = $this->env['GITHUB_RUN_ID'];
         $this->env['CI_BRANCH'] = $githubRef;
 
         $this->readEnv['GITHUB_ACTIONS'] = $this->env['GITHUB_ACTIONS'];
