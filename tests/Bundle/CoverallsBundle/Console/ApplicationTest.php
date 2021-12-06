1d016698b4c5e3dda6be51514275303ce14ae6be
Viktor Linkin <adrenalinkin@gmail.com>
2021-12-06 19:57:09 +0300

    Added compatibility with Symfony 6 (#328)

--- tests/Bundle/CoverallsBundle/Console/ApplicationTest.php
+++ tests/Bundle/CoverallsBundle/Console/ApplicationTest.php
@@ -2,13 +2,11 @@
 
 namespace PhpCoveralls\Tests\Bundle\CoverallsBundle\Console;
 
-use PhpCoveralls\Bundle\CoverallsBundle\Console\Application;
+use PhpCoveralls\Bundle\CoverallsBundle\Console\ApplicationFactory;
 use PhpCoveralls\Tests\ProjectTestCase;
 use Symfony\Component\Console\Tester\ApplicationTester;
 
 /**
- * @covers \PhpCoveralls\Bundle\CoverallsBundle\Console\Application
- *
  * @author Kitamura Satoshi <with.no.parachute@gmail.com>
  */
 class ApplicationTest extends ProjectTestCase
@@ -21,7 +19,7 @@ class ApplicationTest extends ProjectTestCase
         $this->makeProjectDir(null, $this->logsDir);
         $this->dumpCloverXml();
 
-        $app = new Application($this->rootDir, 'Coveralls API client for PHP', '1.0.0');
+        $app = ApplicationFactory::create($this->rootDir);
         $app->setAutoExit(false); // avoid to call exit() in Application
 
         // run
