4d75ffe6c4d74458f8e1011a564d298c8ef9aa47
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-01-03 02:04:50 +0100

    bump

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -27,10 +27,9 @@ $config
         'ternary_to_elvis_operator' => false,
         'visibility_required' => false,
 
-        'no_blank_lines_after_class_opening' => false,
         'blank_line_after_opening_tag' => false,
-        'blank_line_before_statement' => false,
         'no_extra_blank_lines' => false,
+        'single_blank_line_before_namespace' => false,
     ])
     ->setFinder($finder)
 ;
