3b2e02643e8de625d0343f6f06e8823ae64e4c07
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-01-03 02:11:40 +0100

    extra

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -18,14 +18,14 @@ $config
             'identical' => false,
             'less_and_greater' => false,
         ],
-        'concat_space' => false,
+        'concat_space' => ['spacing' => 'one'],
         'explicit_indirect_variable' => false,
         'explicit_string_variable' => false,
         'method_chaining_indentation' => false,
         'php_unit_test_annotation' => false,
         'single_line_comment_style' => false,
         'ternary_to_elvis_operator' => false,
-        'visibility_required' => false,
+        'visibility_required' => ['elements' => ['method', 'property']],
     ])
     ->setFinder($finder)
 ;
